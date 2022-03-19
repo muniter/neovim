7967e130be91d6317c0a31bf181b7a4478bb39b0
Javier López <graulopezjavier@gmail.com>
2022-03-18 23:45:44 -0500

    trigger autogen

--- src/nvim/api/autocmd.c
+++ src/nvim/api/autocmd.c
@@ -1,4 +1,5 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check
+// Generate docs
 // it. PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 #include <stdbool.h>
